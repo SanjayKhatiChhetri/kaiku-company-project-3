2410d24edc98bf5b2ec2fc8dedbe2177ed4d81ef
Elias <t0tuel00@students.oamk.fi>
2024-01-24 17:14:03 +0200

    General observation version 2

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import logo from "../../images/tentrio.png";
-import vector from "../../images/vector.svg";
+import vector from "../images/vector.svg";
 
 export default function Navbar() {
   return (
@@ -8,9 +7,8 @@ export default function Navbar() {
       <div className="navContainer">
         <nav className="navbar">
           <div className="logo">
-            <img src={logo} width={150} height={17} />
           </div>
-          <div>Home</div>
+          <div>YLEISHAVAINNOT</div>
           <div className="threedot">
             <img src={vector} width={26.23} height={24} />
           </div>

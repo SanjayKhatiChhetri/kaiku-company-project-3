6fce06d7abd7cca09f324e65a6c594cf95f3bbdd
Niko Malm <nicodude00@gmail.com>
2024-02-27 10:21:30 +0200

    Added continious button functions

--- src/pages/Projectobservation.js
+++ src/pages/Projectobservation.js
@@ -1,22 +1,110 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Footer from "../component/Footer";
 import { UserIc } from "../Public/Asset/Icons/CostumIcon";
 import  Button  from "@mui/material/Button";
 import vector from "../images/vector.svg"
-import { ToggleButton } from "@mui/material";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { db, storage } from "../component/FirebaseConfig";
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, arrayUnion } from "@firebase/firestore"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function Projectobservation() {
+    const [alignment, setAlignment] = useState();
+    const [clickCount, setClickCount] = useState(0);
+    let [latitude, setlatitude] = useState(null);
+    let [longitude, setlongitude] = useState(null);
+    let [startlatitude, setstartlatitude] = useState(0);
+    let [startlongitude, setstartlongitude] = useState(0);
+    let [endlatitude, setendlatitude] = useState(0);
+    let [endlongitude, setendlongitude] = useState(0);
+    let [name,setName] = useState("")
 
-    function getlocation() {
+
+  
+
+    function getstartlocation() {
         navigator.geolocation.getCurrentPosition((post) => {
             setlatitude((p) => post.coords.latitude);
             setlongitude((p) => post.coords.longitude);
       
-         
-            setLatitude(post.coords.latitude)
-            setLongitude(post.coords.longitude)
+            setstartlatitude(post.coords.latitude);
+            setstartlongitude(post.coords.longitude);
+            
+
+
+        
           });
     }
+
+    function getendlocation() {
+        navigator.geolocation.getCurrentPosition((post) => {
+
+            setlatitude((p) => post.coords.latitude);
+            setlongitude((p) => post.coords.longitude);
+
+            setendlatitude(post.coords.latitude);
+            setendlongitude(post.coords.longitude);
+
+        });
+    }
+
+    const currTime = new Date().toLocaleTimeString(); //Get current time
+    const currDate = new Date().toLocaleDateString(); //Get current date
+
+    const handleChange = (event, newAlignment) => { //Handle togglebutton alignment
+        setAlignment(newAlignment);
+    }
+
+
+    const handleClick = () => {
+        switch(clickCount) {
+
+            case 0:
+                getstartlocation();
+                setClickCount(1);
+                setName("Vesakointipuute")
+                break;
+
+            case 1:
+                getendlocation();    
+                firebaseData();
+                setClickCount(0);
+                break;
+        }
+
+    }
+
+    
+
+
+
+
+    const firebaseData = async() => {
+
+        try {
+            let docRef = await addDoc(collection(db, "data"), {
+                
+                startlat: startlatitude,
+                startlong: startlongitude,
+                endlat: endlatitude,
+                endlong: endlongitude,
+                date: currDate,
+                time: currTime,
+                desc: name,
+
+            })
+
+        }catch(e){
+            console.log(`error adding data to firestore: ${e} `)
+        }
+
+    }
+
+    useEffect(() => {
+        getendlocation()
+      },[]);
+
+
 
     return (
         <div className="projectobs">
@@ -41,13 +129,30 @@ function Projectobservation() {
             <div className="point-btn">
                 <Button variant="contained" size="large" onClick={()=>{
                     alert("clicked")
+                    console.log("aika on : ", currTime)
+                    console.log("Päivä on: ", currDate)
+                    console.log("alkulokaatio", startlatitude)
+                    console.log("loppulokaatio", endlatitude)
                 }}>Pistehavainto</Button>
 
             </div>
 
             <div className="continious-btn">
 
-                <ToggleButton  size="large" color="primary"> Vesakointipuute </ToggleButton>
+                <ToggleButtonGroup
+
+                    color="success"
+                    value={alignment}
+                    exclusive
+                    onChange={handleChange}
+                    aria-label="platform"
+                    
+                >
+
+                    <ToggleButton value="vesakointipuute" onClick={()=> {handleClick()}}> Vesakointipuute </ToggleButton>
+
+
+                </ToggleButtonGroup>
                 
             
             </div>    
